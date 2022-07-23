0aabd4d69e94d3055c495ceee3ac3a87684cc7b3
Siva Vignesh <sivavignesh007.sm@gmail.com>
2022-07-23 16:56:11 +0530

    changes in main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 def read_excel_and_collect_birthday_list():
-    pass
+    return [None]
 
-def open_whatsapp_and_wish(bd_list):
-    pass
+
+def open_whatsapp_and_wish():
+    print(bd_list)
+
 
 if __name__ == "__main__":
     bd_list = read_excel_and_collect_birthday_list()
     if bd_list:
-        open_whatsapp_and_wish(bd_list)
+        open_whatsapp_and_wish()
     else:
         print("Oops!!.. Seems no friends having birthday today")
