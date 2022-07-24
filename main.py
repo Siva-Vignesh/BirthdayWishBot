65de3ca3dbdb43cf0f06ce2fc5862bb21c382cc3
Siva Vignesh <sivavignesh007.sm@gmail.com>
2022-07-24 12:39:04 +0530

    changes in main.py + new csv file for birthday data

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+from datetime import datetime
+
+
 def read_excel_and_collect_birthday_list():
-    return [None]
+    current_day = datetime.now().day
+    current_month = datetime.now().month
+    birthday_list = list()
+    with open("birthday_database.csv", "r") as data:
+        data.readline()  # this will remove the first row (header)
+        for each_row in data.readlines():
+            dob = each_row.split(",")[2]
+            if int(dob.split("-")[0]) == int(current_day) and int(dob.split("-")[1]) == int(current_month):
+                birthday_list.append({"name": each_row.split(',')[1], "ph_no": each_row.split(',')[3]})
+
+    return birthday_list
 
 
 def open_whatsapp_and_wish():
